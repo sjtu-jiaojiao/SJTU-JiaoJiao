7248309f13be88db7a37f1b524393a37a225fe29
Kang Huquan <799385272@qq.com>
2019-07-05 17:28:41 +0800

    add mongo test that auto and no-damaged

--- backend/utils/mongo_test.go
+++ backend/utils/mongo_test.go
@@ -9,14 +9,45 @@ import (
 )
 
 func TestMongoLoad(t *testing.T) {
-	Convey("Load mongodb test", t, func() {
-		collection := mongoDatabase.Collection("picture")
+	Convey("Insert mongodb test", t, func() {
+		collection := mongoDatabase.Collection("test")
 		res, err := collection.InsertOne(mongoContext, bson.M{"name": "pi", "value": 3.14159})
+
 		if err != nil {
 			Error("Insert into mongodb failed.")
 		}
 
 		id := res.InsertedID
-		fmt.Println(id)
+		fmt.Println("insert into database test of id: ", id)
+	})
+	Convey("Select mongodb test", t, func(){
+		collection := mongoDatabase.Collection("test")
+		cur, err := collection.Find(mongoContext, bson.D{})
+		if err != nil { 
+			Error("Select from mongodb failed.")
+		}
+
+		for cur.Next(mongoContext) {
+			var result bson.M
+			err := cur.Decode(&result)
+			if err != nil { 
+				Error("Select from mongodb failed.")
+			}	
+			fmt.Println(result)
+		}
+
+		if err := cur.Err(); err != nil {
+			Error("Select from mongodb failed.")
+		}
+	})
+	Convey("Delete mongodb test", t, func() {
+		collection := mongoDatabase.Collection("test")
+		res, err := collection.DeleteMany(mongoContext, bson.D{{"name", "pi"},},)
+
+		if err != nil {
+			Error("Insert into mongodb failed.")
+		}
+
+		fmt.Println("insert into database test of id: ", res)
 	})
 }
