8a41d71446762de6f678528ed9bd93c7d92bfbea
Kang Huquan <799385272@qq.com>
2019-07-08 16:18:01 +0800

    finish backend auto-manage tool

--- backend/command.go
+++ backend/command.go
@@ -10,7 +10,8 @@ import (
 
 type Status struct {
 	Consul   bool
-	Micro    bool
+	MicroApi bool
+	MicroWeb bool
 	Document bool
 	Goconvey bool
 	Realize  bool
@@ -43,10 +44,15 @@ func main() {
 		case 'C':
 			go startConsul()
 		case 'A':
+			go startMicroApi()
 		case 'W':
+			go startMicroWeb()
 		case 'D':
+			go startServeDoc()
 		case 'G':
+			go startGoConvey()
 		case 'R':
+			go startRealize()
 		}
 	}
 }
@@ -82,12 +88,132 @@ func startConsul() {
 	}
 }
 
+func startMicroApi() {
+	for {
+		time.Sleep(time.Duration(1) * time.Second)
+
+		cmd := exec.Command("micro", "--registry=consul", "api", "--handler=http")
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("start %q error.", cmd)
+			continue
+		}
+
+		fmt.Println("\nMicro api Restart!")
+		status.MicroApi = true
+		printFlag <- true
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("\nMicro api Terminated!")
+			status.MicroApi = false
+			printFlag <- true
+		}
+	}
+}
+func startMicroWeb() {
+	for {
+		time.Sleep(time.Duration(1) * time.Second)
+
+		cmd := exec.Command("micro", "--registry=consul", "web")
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("start %q error.", cmd)
+			continue
+		}
+
+		fmt.Println("\nMicro web Restart!")
+		status.MicroWeb = true
+		printFlag <- true
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("\nMicro web Terminated!")
+			status.MicroWeb = false
+			printFlag <- true
+		}
+	}
+}
+func startServeDoc() {
+	for {
+		time.Sleep(time.Duration(1) * time.Second)
+
+		cmd := exec.Command("go", "run", "servedoc.go", "--registry=consul")
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("start %q error.", cmd)
+			continue
+		}
+
+		fmt.Println("\nServeDoc Restart!")
+		status.Document = true
+		printFlag <- true
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("\nServeDoc Terminated!")
+			status.Document = false
+			printFlag <- true
+		}
+	}
+}
+
+func startGoConvey() {
+	for {
+		time.Sleep(time.Duration(1) * time.Second)
+
+		cmd := exec.Command("goconvey", "-port", "8400", "--launchBrowser=false")
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("start %q error.", cmd)
+			continue
+		}
+
+		fmt.Println("\nGoConvey Restart!")
+		status.Goconvey = true
+		printFlag <- true
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("\nGoConvey Terminated!")
+			status.Goconvey = false
+			printFlag <- true
+		}
+	}
+}
+
+func startRealize() {
+	for {
+		time.Sleep(time.Duration(1) * time.Second)
+
+		cmd := exec.Command("realize", "start")
+		time.AfterFunc(600*time.Second, func() { cmd.Process.Kill() })
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("start %q error.", cmd)
+			continue
+		}
+
+		fmt.Println("\nRealize Restart!")
+		status.Realize = true
+		printFlag <- true
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println("\nRealize Terminated!")
+			status.Realize = false
+			printFlag <- true
+		}
+	}
+}
+
 func showStatus() {
 	for {
 		if <-printFlag {
 			fmt.Println("***Current Status***")
 			fmt.Println("consul \t\t", status.Consul)
-			fmt.Println("micro \t\t", status.Micro)
+			fmt.Println("microApi \t", status.MicroApi)
+			fmt.Println("microWeb \t", status.MicroWeb)
 			fmt.Println("document \t", status.Document)
 			fmt.Println("goconvey \t", status.Goconvey)
 			fmt.Println("realize \t", status.Realize)
