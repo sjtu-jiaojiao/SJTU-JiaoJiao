26445c97d7f650ba110cd5fe678dc999f93f8be2
Jianghao Lin <chiangel.ljh@gmail.com>
2019-07-08 21:28:52 +0800

    upadte Component FileFolder

---
+++ AppFrontend/src/Component/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View } from 'raect-native';
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <View/>
+        )
+    }
+}
---
+++ AppFrontend/src/Component/SearchType.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View } from 'raect-native';
+
+export default class SearchType extends Component {
+    render() {
+        return (
+            <View/>
+        )
+    }
+}
