e7b5cb4b4375cc2a723aa1b9f34b69adfcf7070a
Jianghao Lin <chiangel.ljh@gmail.com>
2019-07-10 02:10:13 +0800

    add Search User Item List

--- AppFrontend/src/Views/Search/index.js
+++ AppFrontend/src/Views/Search/index.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import {SearchBar, CheckBox  } from "react-native-elements";
+import { Text, View, FlatList } from 'react-native';
+import {SearchBar, CheckBox, ListItem} from "react-native-elements";
+
+const DemoUser = [
+    {
+        userName: 'chiangel',
+    },
+    {
+        userName: 'wxz',
+    },
+    {
+        userName: 'Jboss',
+    },
+    {
+        userName: 'Boy',
+    },
+    {
+        userName: 'a',
+    },
+    {
+        userName: 'b',
+    },
+    {
+        userName: 'vc',
+    },
+    {
+        userName: 'd',
+    },
+    {
+        userName: 'e',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+    {
+        userName: 'y',
+    },
+];
+
+class SearchUserItem extends Component {
+    keyExtractor = (item, index) => index.toString()
+
+    renderItem = ({ item }) => (
+        <ListItem
+            bottomDivider
+            title={item.userName}
+            titleStyle={{ color: 'black', fontSize: 17 }}
+        />
+    )
+
+    render() {
+        return (
+            <FlatList
+                keyExtractor={this.keyExtractor}
+                data={DemoUser}
+                renderItem={this.renderItem}
+            />
+        );
+    }
+}
 
 export default class SearchScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
             searchText: '',
-            searchType: 'Sell',
+            searchType: 'User',
+            showSearchItem: false,
         }
     };
 
@@ -17,18 +100,46 @@ export default class SearchScreen extends Component {
 
     updateSearch = (searchText) => {
         this.setState({ searchText });
+        this.setState(previousState => (
+            { showSearchItem: true }
+        ));
+    };
+
+    cancelSearch = (searchText) => {
+        this.setState({ searchText });
+        this.setState(previousState => (
+            {
+                showSearchItem: false,
+                searchText: '',
+            }
+        ));
     };
 
     render() {
         const { searchText } = this.state.searchText;
-        return (
-            <View>
-                <SearchBar
-                    placeholder={'搜索'}
-                    onChangeText={ this.updateSearch }
-                    value={ this.state.searchText }
-                />
-            </View>
-        )
+        if (this.state.showSearchItem) {
+            return (
+                <View>
+                    <SearchBar
+                        placeholder={'搜索'}
+                        onChangeText={ this.updateSearch }
+                        onClear={this.cancelSearch}
+                        value={ this.state.searchText }
+                    />
+                    <SearchUserItem/>
+                </View>
+            )
+        }
+        else {
+            return (
+                <View>
+                    <SearchBar
+                        placeholder={'搜索'}
+                        onChangeText={ this.updateSearch }
+                        value={ this.state.searchText }
+                    />
+                </View>
+            )
+        }
     }
 }
