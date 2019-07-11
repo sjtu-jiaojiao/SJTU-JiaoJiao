f87ffeec5de7b8cf04aa1b294e329b1558b06891
MXWXZ <me@imwxz.com>
2019-07-11 09:09:22 +0800

    travis

--- backend/dockerpush.sh
+++ backend/dockerpush.sh
@@ -1,4 +1,4 @@
-bash docker_push sjtujj/api-auth
-bash docker_push sjtujj/api-user
-bash docker_push sjtujj/srv-auth
-bash docker_push sjtujj/srv-user
+docker push sjtujj/api-auth
+docker push sjtujj/api-user
+docker push sjtujj/srv-auth
+docker push sjtujj/srv-user
