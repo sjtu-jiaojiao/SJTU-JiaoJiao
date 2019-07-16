155fdd1a99ed1e6d4d03a00efa797fb944e26659
sjtuzwj <zwjaaa@hotmail.com>
2019-07-16 19:19:59 +0800

    feat: website info service memrory backend

--- frontend/__tests__/BuyInfo-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BuyInfoScreen from '../src/Views/BuyInfo/BuyInfo';
-
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-    const tree = renderer.create(<BuyInfoScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
--- frontend/__tests__/Home-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HomeScreen from '../src/Views/Home/Home';
-
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-    const tree = renderer.create(<HomeScreen/>).toJSON();
-    expect(tree).toMatchSnapshot();
-})
