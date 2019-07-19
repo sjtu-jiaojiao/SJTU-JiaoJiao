5734592c7ff6990216372beac19a203558539a8c
Xinzhe Wang <me@imwxz.com>
2019-07-19 22:12:32 +0800

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,14 +2,12 @@ CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/api-auth ./ap
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/api-user ./api/user/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/api-sellinfo ./api/sellinfo/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/api-buyinfo ./api/buyinfo/main.go
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/api-site ./api/site/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/api-file ./api/file/main.go
 # build api insert before this
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/srv-auth ./srv/auth/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/srv-user ./srv/user/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/srv-sellinfo ./srv/sellinfo/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/srv-buyinfo ./srv/buyinfo/main.go
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/srv-site ./srv/site/main.go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./build/srv-file ./srv/file/main.go
 # build srv insert before this
 
