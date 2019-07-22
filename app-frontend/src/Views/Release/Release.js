af60422b23ccb9b0b50b333aa6ba5ec6a5746be4
Jianghao Lin <chiangel.ljh@gmail.com>
2019-07-22 14:35:19 +0800

    Reload ReleaseScreen every time press the tab

--- app-frontend/src/Views/Release/Release.js
+++ app-frontend/src/Views/Release/Release.js
@@ -10,6 +10,10 @@ export default class ReleaseScreen extends Component {
     };
 
     render() {
+
+        // 有一个比较严重的bug，如果先登录，进入发布界面，之后再退出，这里的Config.userInfo.userId是不会变回-1的！
+        // 要想办法使这里重新加载Config，或者使用Redux
+
         if (Config.userInfo.userId === -1) {
             Alert.alert(
                 '未登录',
@@ -18,25 +22,27 @@ export default class ReleaseScreen extends Component {
                     {
                         text: '取消',
                         onPress: () => {
-                            this.props.navigation.reset([NavigationActions.navigate({ routeName: 'Home' })], 0);
+                            this.props.navigation.navigate('Home');
                         },
                         style: 'cancel',
                     },
                     {
                         text: '确定', onPress: () => {
-                            this.props.navigation.reset([NavigationActions.navigate({ routeName: 'Login' })], 0);
+                            this.props.navigation.navigate('Login');
                         }
                     },
                 ],
                 {cancelable: false},
             )
+        } else {
+            return (
+                <View>
+                    <Text>
+                        {Config.userInfo.userId}
+                    </Text>
+                </View>
+            )
         }
-        return (
-            <View>
-                <Text>
-                    {Config.userInfo.userId}
-                </Text>
-            </View>
-        )
+        return (<View/>);
     }
 }
