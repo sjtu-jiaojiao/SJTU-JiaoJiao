a335d0981cb0401c719fbba9ef04397a9f598207
Kang Huquan <799385272@qq.com>
2019-07-24 14:36:50 +0800

    transaction srv

--- backend/srv/transaction/main.go
+++ backend/srv/transaction/main.go
@@ -12,10 +12,6 @@ import (
 
 type srv struct{}
 
-func (a *srv) Find(context.Context, *transaction.TransactionFindRequest, *transaction.TransactionFindResponse) error {
-	panic("implement me")
-}
-
 /**
  * @api {rpc} /rpc Transaction.Create
  * @apiVersion 1.0.0
@@ -88,6 +84,74 @@ func (a *srv) Update(ctx context.Context, req *transaction.TransactionUpdateRequ
 	}
 
 	rsp.Status = transaction.TransactionUpdateResponse_SUCCESS
+	return nil
+}
+
+/**
+ * @api {rpc} /rpc Transaction.Find
+ * @apiVersion 1.0.0
+ * @apiGroup Service
+ * @apiName Transaction.Find
+ * @apiDescription Find transactions
+ *
+ * @apiParam {int32} [infoID] sellInfoID or buyInfoID.
+ * @apiParam {int32} [category] 1 for sell <br> 2 for buy
+ * @apiParam {int32} [userID] userID whose create the transaction
+ * @apiParam {int64} [lowCreateTime] low boundary of CreateTime
+ * @apiParam {int64} [highCreateTime] high boundary of CreateTime
+ * @apiParam {int32} [status] 1 for order <br> 2 for done
+ * @apiParam {uint32} limit=100 row limit
+ * @apiParam {uint32} offset=0 row offset
+ * @apiSuccess {int32} status -1 for invalid param <br> 1 for success <br> 2 for not found
+ * @apiSuccess {array} transactions see below
+ * @apiSuccess (Transaction Array) {int32} transactionID transaction ID
+ * @apiSuccess (Transaction Array) {int32} infoID sellInfoID or buyInfoID
+ * @apiSuccess (Transaction Array) {int32} category 1 for sell <br> 2 for buy
+ * @apiSuccess (Transaction Array) {int32} userID userID whose create the transaction
+ * @apiSuccess (Transaction Array) {int64} createTime create time
+ * @apiSuccess (Transaction Array) {int32} status 1 for order <br> 2 for done
+ * @apiUse DBServerDown
+ */
+func (a *srv) Find(ctx context.Context, req *transaction.TransactionFindRequest, rsp *transaction.TransactionFindResponse) error {
+	if req.Limit == 0 {
+		req.Limit = 100
+	}
+
+	var res []*db.Transaction
+	tx := db.Ormer.Table("transactions")
+	if req.InfoID != 0 {
+		tx.Where("info_id = ?", req.InfoID)
+	}
+	if req.Category != transaction.TransactionFindRequest_CATEGORY_UNKNOWN {
+		tx.Where("category = ?", int32(req.Category))
+	}
+	if req.UserID != 0 {
+		tx.Where("user_id = ?", req.UserID)
+	}
+	if req.LowCreateTime != 0 {
+		tx.Where("create_time > ?", time.Unix(req.LowCreateTime, 0))
+	}
+	if req.HighCreateTime != 0 {
+		tx.Where("create_time < ?", time.Unix(req.HighCreateTime, 0))
+	}
+	if req.Status != transaction.TransactionFindRequest_STATUS_UNKNOWN {
+		tx.Where("status = ?", int32(req.Status))
+	}
+	err := tx.Limit(req.Limit).Offset(req.Offset).Find(&res).Error
+	if utils.LogContinue(err, utils.Warning) {
+		rsp.Status = transaction.TransactionFindResponse_NOT_FOUND
+		return nil
+	}
+	for i, v := range res {
+		rsp.Transactions = append(rsp.Transactions, new(transaction.TransactionMsg))
+		rsp.Transactions[i].TransactionID = v.ID
+		rsp.Transactions[i].InfoID = v.InfoID
+		rsp.Transactions[i].Category = transaction.TransactionMsg_Category(v.Category)
+		rsp.Transactions[i].UserID = v.UserID
+		rsp.Transactions[i].CreateTime = v.CreateTime.Unix()
+		rsp.Transactions[i].Status = transaction.TransactionMsg_Status(v.Status)
+	}
+	rsp.Status = transaction.TransactionFindResponse_SUCCESS
 	return nil
 }
 
