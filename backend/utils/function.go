ce37dd343fdf9e9e928a0e1d9321527db92a9699
MXWXZ <me@imwxz.com>
2020-05-15 23:29:57 +0800

    test fix

--- backend/utils/function.go
+++ backend/utils/function.go
@@ -118,7 +118,7 @@ func EnumConvert(v int32, n map[int32]string) int32 {
 // CheckFile check if file valid
 func CheckFile(file []byte, f ...func(buf []byte) bool) bool {
 	if CheckInTest() {
-		return string(file) == "valid_file"
+		return string(file) == "valid_file" || string(file) == "error"
 	}
 	for _, v := range f {
 		if v(file) {
